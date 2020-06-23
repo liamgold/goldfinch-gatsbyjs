f0ad242f35f80345de737bb73de4fc9f09aa1bc1
Liam Goldfinch <liamwoody323@gmail.com>
2020-06-23 21:01:56 +0100

    changed google fonts to be self hosted

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,16 +2,6 @@ import Typography from 'typography';
 
 const typography = new Typography({
   bodyFontFamily: ['Open Sans', 'sans-serif'],
-  googleFonts: [
-    {
-      name: 'Roboto',
-      styles: ['700'],
-    },
-    {
-      name: 'Open Sans',
-      styles: ['400', '400i', '700', '700i'],
-    },
-  ],
   headerFontFamily: ['Roboto', 'Arial', 'sans-serif'],
   scaleRatio: 1.4,
 });
