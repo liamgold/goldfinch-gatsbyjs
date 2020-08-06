94985684585f6c3e98f2b8fcfcd74c5447875543
Liam Goldfinch <liamwoody323@gmail.com>
2020-08-06 22:39:16 +0100

    Added robots.txt and sitemap.xml gatsbyjs plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,28 @@
-const path = require('path');
+const path = require(`path`);
 
-require('dotenv').config({
+require(`dotenv`).config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.goldfinch.me`,
+  },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-plugin-typography',
+      resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: 'src/utils/typography.js',
+        pathToConfigModule: `src/utils/typography.js`,
       },
     },
     {
-      resolve: '@kentico/gatsby-source-kontent',
+      resolve: `@kentico/gatsby-source-kontent`,
       options: {
         projectId: process.env.API_KEY,
-        languageCodenames: ['default'],
+        languageCodenames: [`default`],
       },
     },
     {
@@ -27,30 +32,30 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-100971234-1',
+        trackingId: `UA-100971234-1`,
         head: true,
         anonymize: true,
         respectDNT: true,
       },
     },
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Liam Goldfinch - .NET Developer',
-        short_name: 'Goldfinch',
-        start_url: '/',
-        background_color: '#FFFFFF',
-        theme_color: '#444444',
+        name: `Liam Goldfinch - .NET Developer`,
+        short_name: `Goldfinch`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#444444`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone',
-        icon: 'src/assets/icon.png',
+        display: `standalone`,
+        icon: `src/assets/icon.png`,
       },
     },
     // If you’re using this plugin with gatsby-plugin-manifest (recommended) this plugin should be listed
     // after that plugin so the manifest file can be included in the service worker.
-    'gatsby-plugin-offline',
+    `gatsby-plugin-offline`,
   ],
 };
