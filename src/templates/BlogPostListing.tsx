0f4a7eb10d3d4da57eeff5ab95e4e19fd2417fd7
Liam Goldfinch <liamwoody323@gmail.com>
2020-10-01 00:06:10 +0100

    added pagination to blog listing

--- src/templates/BlogPostListing.tsx
+++ src/templates/BlogPostListing.tsx
@@ -6,9 +6,18 @@ import Layout from '@components/Layout';
 import { BlogListing } from '@models/BlogListing';
 import { BlogDetail } from '@models/BlogDetail';
 import BlogCard from '@components/blog/BlogCard';
+import Pagination from '@components/shared/Pagination';
 
 interface BlogListProps {
   data: BlogListResult;
+  pageContext: BlogListPageContext;
+}
+
+interface BlogListPageContext {
+  currentPage: number;
+  limit: number;
+  numPages: number;
+  skip: number;
 }
 
 interface BlogListResult {
@@ -30,13 +39,15 @@ const BlogList = styled.div`
   justify-content: space-between;
 `;
 
-const Index: FunctionComponent<BlogListProps> = ({ data }) => {
+const BlogPostListing: FunctionComponent<BlogListProps> = ({ data, pageContext }) => {
   const item = data.kontentItemBlogListing;
   const blogDetails = data.allKontentItemBlogDetail;
 
   if (!item) {
     return null;
   }
+
+  const { currentPage, numPages } = pageContext;
 
   return (
     <Layout title={item?.elements?.seo__meta_title?.value} description={item?.elements?.seo__meta_description?.value}>
@@ -49,15 +60,16 @@ const Index: FunctionComponent<BlogListProps> = ({ data }) => {
             return <BlogCard key={blog.system.id} blog={blog} />;
           })}
         </BlogList>
+        <Pagination baseUrl="/blog/" currentPage={currentPage} numPages={numPages} />
       </div>
     </Layout>
   );
 };
 
-export default Index;
+export default BlogPostListing;
 
 export const query = graphql`
-  query BlogListQuery {
+  query BlogListQuery($skip: Int!, $limit: Int!) {
     kontentItemBlogListing {
       elements {
         base__title {
@@ -71,7 +83,7 @@ export const query = graphql`
         }
       }
     }
-    allKontentItemBlogDetail(sort: { fields: elements___post_date___value, order: DESC }) {
+    allKontentItemBlogDetail(sort: { fields: elements___post_date___value, order: DESC }, limit: $limit, skip: $skip) {
       edges {
         node {
           elements {
